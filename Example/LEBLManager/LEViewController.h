f3410558e4330258327d3c29181bea9f509e565f
leonStraightMen <dengzf.ok@163.com>
2021-06-21 16:15:54 +0800

    Initial commit

---
+++ Example/LEBLManager/LEViewController.h
@@ -0,0 +1,13 @@
+//
+//  LEViewController.h
+//  LEBLManager
+//
+//  Created by leonStraightMen on 06/21/2021.
+//  Copyright (c) 2021 leonStraightMen. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LEViewController : UIViewController
+
+@end
---
+++ Example/LEBLManager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LEBLManager
+//
+//  Created by leonStraightMen on 06/21/2021.
+//  Copyright (c) 2021 leonStraightMen. All rights reserved.
+//
+
+@import UIKit;
+#import "LEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LEAppDelegate class]));
+    }
+}
